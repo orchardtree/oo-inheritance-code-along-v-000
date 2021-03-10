3655cd970d95bbef19e094c13684fef2d51e813e
Jesse Wenger <wengerj@gmail.com>
2021-03-10 18:18:19 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,6 +1,6 @@
 class Vehicle
-  def initialize(size, number)
-    @size = size 
-    @number = number
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size 
+    @wheel_number = wheel_number
   end
 end
